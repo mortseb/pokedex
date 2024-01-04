2db36d147425f9cb83ef2999a0760d1605de4a09
Mortseb <sebmortiers.95@hotmail.com>
2024-01-04 12:17:32 +0100

    poke_slug

--- pages/pokemon/[slug].vue
+++ pages/pokemon/[slug].vue
@@ -27,7 +27,11 @@ query Pokemon($slug: String!) {
 `;
 
 const pokemon = ref();
+const sectionVisible = ref(null);
 
+const showSection = (section) => {
+  sectionVisible.value = section;
+};
 const route = useRoute();
 const { data } = await useAsyncQuery(query, {
   slug: route.params.slug,
@@ -40,13 +44,97 @@ console.log(data.value.pokemon);
 </script>
 
 <template>
-  <div v-if="pokemon" class="max-w-lg">
-    <NuxtImg :src="pokemon.artwork.url" :alt="pokemon.nom" />
-    <h2 class="text-3xl text-center text-white">{{ pokemon.nom }}</h2>
-    <div class="text-white">Desciption : {{ pokemon.description }}</div>
+  <div v-if="pokemon" class="flex flex-row w-full bg-slate-200 py-6 sm:py-12 mt-8">
+    <div class="w-1/3">
+
+      <NuxtImg :src="pokemon.artwork.url" :alt="pokemon.nom" class="h-full w-full" />
+    </div>
+    <div class="flex flex-col w-2/3">
+      <div class=" flex justify-center items-start">
+        <button @click="showSection('description')"
+          class=" text-black group flex items-center justify-center rounded-lg bg-slate-200 px-1 py-1 md:px-3 md:px-3 mx-2 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+          <span class="sr-only">Home</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+            class="h-4 w-4 md:w-6 md:h-6 text-slate-500 group-active:scale-95">
+            <path
+              d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+            <path
+              d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+          </svg>
+        </button>
+        <button @click="showSection('attaques')"
+          class="group flex items-center justify-center rounded-lg bg-slate-200 px-1 py-1 md:px-3 md:px-3 mx-2 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+          <span class="sr-only">Account</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+            class="h-4 w-4 md:w-6 md:h-6 text-slate-500 group-active:scale-95">
+            <path fill-rule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+
+        <button @click="showSection('localisation')"
+          class="flex group flex items-center justify-center rounded-lg bg-slate-200 px-1 py-1 md:px-3 md:px-3 mx-2 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+          <span class="sr-only">Explore</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+            class="h-4 w-4 md:w-6 md:h-6 text-slate-500 group-active:scale-95">
+            <path fill-rule="evenodd"
+              d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+
+        <button @click="showSection('informations')"
+          class="group flex items-center justify-center rounded-lg bg-slate-200 px-1 py-1 md:px-3 md:px-3 mx-2 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+          <span class="sr-only">Notifications</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+            class="h-4 w-4 md:w-6 md:h-6 text-slate-500 group-active:scale-95">
+            <path
+              d="M5.85 3.5a.75.75 0 00-1.117-1 9.719 9.719 0 00-2.348 4.876.75.75 0 001.479.248A8.219 8.219 0 015.85 3.5zM19.267 2.5a.75.75 0 10-1.118 1 8.22 8.22 0 011.987 4.124.75.75 0 001.48-.248A9.72 9.72 0 0019.266 2.5z" />
+            <path fill-rule="evenodd"
+              d="M12 2.25A6.75 6.75 0 005.25 9v.75a8.217 8.217 0 01-2.119 5.52.75.75 0 00.298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 107.48 0 24.583 24.583 0 004.83-1.244.75.75 0 00.298-1.205 8.217 8.217 0 01-2.118-5.52V9A6.75 6.75 0 0012 2.25zM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 004.496 0l.002.1a2.25 2.25 0 11-4.5 0z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+        <button @click="showSection('pays')"
+          class="group flex items-center justify-center rounded-lg bg-slate-200 px-1 py-1 md:px-3 md:px-3 mx-2 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+          <span class="sr-only">Settings</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+            class="h-4 w-4 md:w-6 md:h-6 text-slate-500 group-active:scale-95">
+            <path fill-rule="evenodd"
+              d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <div v-if="sectionVisible === 'description'">
+          <h3> description</h3>
+        </div>
+        <div v-if="sectionVisible === 'attaques'">
+          <h3> attaques</h3>
+        </div>
+        <div v-if="sectionVisible === 'localisation'">
+          <h3> localisation</h3>
+        </div>
+        <div v-if="sectionVisible === 'informations'">
+          <h3> informations</h3>
+        </div>
+        <div v-if="sectionVisible === 'pays'">
+          <h3> pays</h3>
+        </div>
+      </div>
+    </div>
   </div>
+
 
   <div v-else>
     <li>Loading...</li>
   </div>
+
+  <!--
+  Neumorphic Buttons
+  Created by Surjith S M 
+  https://twitter.com/surjithctly 
+-->
 </template>
